9b4ec2631a35a47ff2a9020fc5343dc8cf6d91bc
blackstar217 <blackstar.test.toptal@outlook.com>
2018-06-05 03:44:06 +0800

    Android config

--- android/app/src/main/java/com/rntest/MainApplication.java
+++ android/app/src/main/java/com/rntest/MainApplication.java
@@ -3,6 +3,8 @@ package com.rntest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.auth0.lock.react.LockReactPackage;
+import com.auth0.react.A0Auth0Package;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LockReactPackage(),
+            new A0Auth0Package(),
             new VectorIconsPackage()
       );
     }
